9990e634a202fdcd2bb41b7fff4fc1db884b8e83
Ayyah-A <aayyah2@gmail.com>
2021-03-05 23:52:37 -0500

    IVG Website

---
+++ ivg/script.js
@@ -0,0 +1,12 @@
+
+/*var date = new Date();
+document.body.innerHTML = "<h3>The date today is " + (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear() + "</h3>";*/
+
+//alert('Hello Glow Gals');
+
+//Selecting elements
+let emailElement = document.querySelector('#email');
+let submitButton = document.querySelector('#submit-button');
+
+console.log(emailElement)
+console.log(submitButton)
