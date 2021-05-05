771c64121c1520d18b72123d2a21307b1e745846
Ayyah-A <aayyah2@gmail.com>
2021-05-05 05:16:38 -0400

    cc

--- ivg/script.js
+++ ivg/script.js
@@ -4,6 +4,22 @@ document.body.innerHTML = "<h3>The date today is " + (date.getMonth() + 1) + "/"
 
 //alert('Hello Glow Gals');
 
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    });
+}
+
+navSlide();
+
+
+
+ 
+
+
 //Selecting elements
 let emailElement = document.querySelector('#email');
 let submitButton = document.querySelector('#submit-button');
