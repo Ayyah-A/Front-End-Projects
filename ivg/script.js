8abdc91df01dc1cbedd3026ea1362389ac21403f
Ayyah-A <aayyah2@gmail.com>
2021-05-10 23:29:22 -0400

    nav bar adjustments

--- ivg/script.js
+++ ivg/script.js
@@ -7,10 +7,26 @@ document.body.innerHTML = "<h3>The date today is " + (date.getMonth() + 1) + "/"
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li')
 
     burger.addEventListener('click', () => {
+        //Toggle Now
         nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index)=> {
+            if(link.style.animation){
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle('toggle'); 
+
     });
+
+   
 }
 
 navSlide();
